0e4e669fa8f9d6d8fcac6ea52f821ec4dee48157
Tseng Shih Yang <iamdog43gmail.com>
2021-11-15 13:43:07 +0800

    建立資料表

---
+++ member/include/nav.php
@@ -0,0 +1,10 @@
+<nav>
+
+
+<a href="/member/memcenter.php">會員中心</a>
+<a href="/member/forgot.php">忘記密碼</a>
+<a href="/member/edit_user.php">編輯會員資料</a>
+<a href="/member/reg.php">註冊新會員</a>
+<a href="/member/login.php">點我登入</a>
+
+    </nav>
---
+++ member/include/sidebar.php
@@ -0,0 +1,11 @@
+     
+<div class="sidebar">
+    <ul>
+    <li>功能一</li>
+    <li>功能二</li>
+    <li>功能三</li>
+    <li>功能四</li>
+    <li>功能五</li>
+    
+    </ul>
+</div>
